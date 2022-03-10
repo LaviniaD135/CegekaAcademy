e5dd702b87d3bc0c613134fd2a584d4e2e523fd2
LaviniaD135 <63166074+LaviniaD135@users.noreply.github.com>
2022-03-10 19:10:52 +0200

    Add project files.

---
+++ Tema01_academy/Program.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Tema01_academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set<int> obj = new Set<int>();
+            try
+            {
+                obj.Insert(1);
+                obj.Insert(2);
+                obj.Insert(2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"!!!!{ex.Message}!!!");
+            }
+            Console.WriteLine($"------Setul obj1 dupa adaugarea de elemente: ------");
+            obj.Display();
+            obj.Remove(2);
+            Console.WriteLine($"------Setul obj1 dupa stergerea elementului 2------");
+            obj.Display();
+            Set<string> obj2 = new Set<string>();
+            try
+            {
+                obj2.Insert("Ana");
+                obj2.Insert("Banana");
+                obj2.Insert("Lalala");
+                obj2.Insert("Tralalala");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"------{ex.Message}------");
+            }
+
+            Set<string> obj3 = new Set<string>();
+            try
+            {
+                obj3.Insert("alabala");
+                obj3.Insert("portocala");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            try
+            {
+                Console.WriteLine($"------Setul 2+ setul 3: ------");
+                obj2.Merge(obj3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            obj2.Display();
+            Console.WriteLine($"------Subset filtrat: ------");
+            obj2.Filter(t => (t.ToString()).Contains("n"));
+
+        }
+
+    }
+}
---
+++ Tema01_academy/Set.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema01_academy
+{
+    public class Set<T>
+    {
+        List<T> list = new List<T>();
+
+        public void Insert(T item)
+        {
+            if (Contains(item))
+            {
+                throw new Exception("Setul nu poate contine valori duplicat!");
+            }
+            else
+            {
+                list.Add(item);
+            }
+
+        }
+        public void Remove(T item)
+        {
+            list.Remove(item);
+
+        }
+        public bool Contains(T item)
+        {
+            foreach (T item2 in list)
+            {
+                if (item2.Equals(item))
+                    return true;
+
+            }
+            return false;
+        }
+        public void Display()
+        {
+            foreach (T item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public Set<T> Merge(Set<T> other)
+        {
+            foreach (T item in other.list)
+            {
+                if (Contains(item))
+                {
+                    throw new Exception("Setul rezultat nu poate contine valori duplicat!");
+
+                }
+            }
+            list.AddRange(other.list);
+            return this;
+        }
+        public Set<T> Filter(Predicate<T> predicate)
+        {
+            Set<T> subset = new Set<T>();
+            subset.list = list.FindAll(predicate);
+            subset.Display();
+            return subset;
+        }
+        public List<T> List
+        {
+            get
+            {
+                return list;
+            }
+            set
+            {
+                list = value;
+            }
+        }
+
+    }
+}
